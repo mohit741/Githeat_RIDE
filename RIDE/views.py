da44a3bafa9a7a64bdac12a2aad9ca07b010711d
mohit741 <mohit.verma741@gmail.com>
2018-06-03 13:47:56 +0530

    Fixed bugs in JSON output

--- RIDE/views.py
+++ RIDE/views.py
@@ -22,9 +22,9 @@ def index(request):
             train_name = train + ' ' + train_meta
             table = soup.find('table', class_='results')
             df = pd.read_html(str(table))
-            res = df[0].to_json(orient='records')
+            res = df[0].to_json()
             data['train_name'] = train_name
-            data['stations'] = res.strip('[').strip(']')
+            data['stations'] = json.loads(res)
             li = soup.find('ul', class_='list-unstyled info-summary').find_all('li')
             details = {}
             for i in range(0, 2):
@@ -40,8 +40,8 @@ def index(request):
             soup = BeautifulSoup(r2.text, 'html.parser')
             table = soup.find('div', class_='panel panel-warning').find('table')
             df = pd.read_html(str(table))
-            res = df[0].to_json(orient='records')
-            data['fare'] = res.strip('[').strip(']')
+            res = df[0].to_json()
+            data['fare'] = json.loads(res)
             return HttpResponse(json.dumps(data), content_type='application/json')
 
         except Exception as e:
